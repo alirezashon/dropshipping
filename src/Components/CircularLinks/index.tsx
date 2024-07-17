8448d9b63427c6a647c3620d6086b4d53596093c
alirezashon <alirezafeshki2017@gmail.com>
2024-07-18 00:51:56 +0330

    start with me

---
+++ src/Components/CircularLinks/index.tsx
@@ -0,0 +1,106 @@
+import { useEffect, useRef, useState } from 'react'
+import Image from 'next/image'
+import styles from './index.module.css'
+import Link from 'next/link'
+import { Category } from '@/Interfaces'
+
+interface Props {
+  data: Category[]
+}
+
+const CircularLinks: React.FC<Props> = ({ data }) => {
+  const marqueeRef = useRef<HTMLDivElement>(null)
+  const [isScrolling, setIsScrolling] = useState(true) // State to control scrolling
+  const [scrollDirection, setScrollDirection] = useState<
+    'forward' | 'backward'
+  >('forward')
+
+  useEffect(() => {
+    const marquee = marqueeRef.current
+
+    let scrollInterval: NodeJS.Timeout | null = null
+
+    const startScrolling = () => {
+      if (marquee) {
+        scrollInterval = setInterval(() => {
+          if (scrollDirection === 'forward') {
+            marquee.scrollLeft += 1
+            if (marquee.scrollLeft >= marquee.scrollWidth) {
+              setScrollDirection('backward')
+            }
+          } else {
+            marquee.scrollLeft -= 1
+            if (marquee.scrollLeft <= 0) {
+              setScrollDirection('forward')
+            }
+          }
+        }, 0.00001) 
+      }
+    }
+
+    if (isScrolling) {
+      startScrolling()
+    }
+
+    return () => {
+      if (scrollInterval) clearInterval(scrollInterval)
+    }
+  }, [isScrolling, scrollDirection])
+
+  // Pause scrolling when mouse enters the marquee area
+  const handleMouseEnter = () => {
+    setIsScrolling(false)
+  }
+
+  // Resume scrolling when mouse leaves the marquee area
+  const handleMouseLeave = () => {
+    setIsScrolling(true)
+  }
+
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.marquee}
+        ref={marqueeRef}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        {data?.map((item, index) => (
+          <Link
+            key={index}
+            href={`/Store/#${item.name}`}
+            className={styles.name}
+          >
+            <Image
+              src={`${item.src}`}
+              alt={item.name}
+              className={styles.image}
+              width={99}
+              height={99}
+            />
+            <h6>{item.name}</h6>
+          </Link>
+        ))}
+        {/* Render items again to create seamless loop */}
+        {data?.map((item, index) => (
+          <Link
+            key={index + data.length}
+            href={`/Store/#${item.name}`}
+            className={styles.name}
+          >
+            <Image
+              src={`${item.src}`}
+              alt={item.name}
+              className={styles.image}
+              width={99}
+              height={99}
+            />
+            <h6>{item.name}</h6>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default CircularLinks
---
+++ src/lib/cateegories/index.ts
@@ -0,0 +1,248 @@
+import { Category } from "@/Interfaces";
+
+export const categories: Category[] = [
+    {
+      name: 'Food',
+      src: '/images/iconn.png',
+      keywords: ['groceries', 'produce', 'snacks', 'beverages', 'meat'],
+    },
+    {
+      name: 'Home Appliances',
+      src: '/images/iconn.png',
+      keywords: [
+        'kitchen',
+        'cleaning',
+        'refrigerator',
+        'microwave',
+        'washing machine',
+      ],
+    },
+    {
+      name: 'Computer & Office',
+      src: '/images/iconn.png',
+      keywords: ['laptop', 'desktop', 'printer', 'office supplies', 'networking'],
+    },
+    {
+      name: 'Home Improvement',
+      src: '/images/iconn.png',
+      keywords: ['tools', 'hardware', 'painting', 'plumbing', 'electrical'],
+    },
+    {
+      name: 'Home & Garden',
+      src: '/images/iconn.png',
+      keywords: ['furniture', 'decor', 'gardening', 'plants', 'outdoor'],
+    },
+    {
+      name: 'Sports & Entertainment',
+      src: '/images/iconn.png',
+      keywords: ['fitness', 'sports equipment', 'games', 'music', 'movies'],
+    },
+    {
+      name: 'Office & School Supplies',
+      src: '/images/iconn.png',
+      keywords: ['stationery', 'notebooks', 'pens', 'binders', 'school'],
+    },
+    {
+      name: 'Toys & Hobbies',
+      src: '/images/iconn.png',
+      keywords: ['toys', 'games', 'hobbies', 'puzzles', 'collectibles'],
+    },
+    {
+      name: 'Security & Protection',
+      src: '/images/iconn.png',
+      keywords: ['safety', 'surveillance', 'alarms', 'locks', 'protective gear'],
+    },
+    {
+      name: 'Automobiles, Parts & Accessories',
+      src: '/images/iconn.png',
+      keywords: [
+        'car parts',
+        'car accessories',
+        'maintenance',
+        'tires',
+        'car electronics',
+      ],
+    },
+    {
+      name: 'Jewelry & Accessories',
+      src: '/images/iconn.png',
+      keywords: ['rings', 'necklaces', 'bracelets', 'earrings', 'watches'],
+    },
+    {
+      name: 'Lights & Lighting',
+      src: '/images/iconn.png',
+      keywords: ['LED', 'bulbs', 'lamps', 'fixtures', 'outdoor lighting'],
+    },
+    {
+      name: 'Consumer Electronics',
+      src: '/images/iconn.png',
+      keywords: [
+        'smartphones',
+        'tablets',
+        'headphones',
+        'cameras',
+        'accessories',
+      ],
+    },
+    {
+      name: 'Beauty & Health',
+      src: '/images/iconn.png',
+      keywords: ['skincare', 'makeup', 'haircare', 'wellness', 'supplements'],
+    },
+    {
+      name: 'Weddings & Events',
+      src: '/images/iconn.png',
+      keywords: [
+        'wedding dresses',
+        'party supplies',
+        'decorations',
+        'favors',
+        'gifts',
+      ],
+    },
+    {
+      name: 'Shoes',
+      src: '/images/iconn.png',
+      keywords: ['sneakers', 'boots', 'heels', 'sandals', 'formal shoes'],
+    },
+    {
+      name: 'Electronic Components & Supplies',
+      src: '/images/iconn.png',
+      keywords: [
+        'resistors',
+        'capacitors',
+        'transistors',
+        'integrated circuits',
+        'sensors',
+      ],
+    },
+    {
+      name: 'Phones & Telecommunications',
+      src: '/images/iconn.png',
+      keywords: ['smartphones', 'accessories', 'SIM cards', 'routers', 'modems'],
+    },
+    {
+      name: 'Tools',
+      src: '/images/iconn.png',
+      keywords: [
+        'hand tools',
+        'power tools',
+        'tool sets',
+        'garden tools',
+        'repair kits',
+      ],
+    },
+    {
+      name: 'Mother & Kids',
+      src: '/images/iconn.png',
+      keywords: ['baby', 'toddler', 'clothing', 'toys', 'parenting'],
+    },
+    {
+      name: 'Furniture',
+      src: '/images/iconn.png',
+      keywords: ['sofas', 'beds', 'tables', 'chairs', 'storage'],
+    },
+    {
+      name: 'Watches',
+      src: '/images/iconn.png',
+      keywords: [
+        'wristwatches',
+        'smartwatches',
+        'luxury watches',
+        'watch accessories',
+        'timepieces',
+      ],
+    },
+    {
+      name: 'Luggage & Bags',
+      src: '/images/iconn.png',
+      keywords: ['suitcases', 'backpacks', 'handbags', 'travel bags', 'wallets'],
+    },
+    {
+      name: "Women's Clothing",
+      src: '/images/iconn.png',
+      keywords: ['dresses', 'tops', 'skirts', 'pants', 'outerwear'],
+    },
+    {
+      name: "Men's Clothing",
+      src: '/images/iconn.png',
+      keywords: ['shirts', 'pants', 'suits', 'jackets', 'casual wear'],
+    },
+    {
+      name: 'Apparel Accessories',
+      src: '/images/iconn.png',
+      keywords: ['belts', 'hats', 'scarves', 'gloves', 'sunglasses'],
+    },
+    {
+      name: 'Hair Extensions & Wigs',
+      src: '/images/iconn.png',
+      keywords: [
+        'wigs',
+        'hairpieces',
+        'extensions',
+        'hair care',
+        'styling tools',
+      ],
+    },
+    {
+      name: 'Special Category',
+      src: '/images/iconn.png',
+      keywords: [
+        'unique',
+        'limited edition',
+        'rare items',
+        'special offers',
+        'exclusive',
+      ],
+    },
+    {
+      name: 'Underwear',
+      src: '/images/iconn.png',
+      keywords: ['lingerie', 'bras', 'panties', 'boxers', 'socks'],
+    },
+    {
+      name: 'Novelty & Special Use',
+      src: '/images/iconn.png',
+      keywords: [
+        'costumes',
+        'fun',
+        'gag gifts',
+        'themed items',
+        'party supplies',
+      ],
+    },
+    {
+      name: 'Virtual Products',
+      src: '/images/iconn.png',
+      keywords: [
+        'e-books',
+        'software',
+        'online courses',
+        'subscriptions',
+        'gift cards',
+      ],
+    },
+    {
+      name: 'Sports Shoes, Clothing & Accessories',
+      src: '/images/iconn.png',
+      keywords: [
+        'running shoes',
+        'activewear',
+        'sports gear',
+        'fitness accessories',
+        'athletic clothing',
+      ],
+    },
+    {
+      name: 'Motorcycle Equipments & Parts',
+      src: '/images/iconn.png',
+      keywords: [
+        'helmets',
+        'gloves',
+        'riding gear',
+        'motorcycle parts',
+        'accessories',
+      ],
+    },
+  ]
+  
